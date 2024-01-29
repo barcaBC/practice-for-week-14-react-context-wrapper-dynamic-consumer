5d8ec555a43e5bc48c72444957b76762a2529eea
Barca <143180305+barcaBC@users.noreply.github.com>
2024-01-29 13:17:58 +0000

    zumba

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,12 +1,13 @@
-import { useContext } from 'react';
-import { HoroscopeContext } from '../context/HoroscopeContext';
+import { useContext } from "react";
+import { HoroscopeContext } from "../context/HoroscopeContext";
 
 const Detail = () => {
   const { sign } = useContext(HoroscopeContext);
+  console.log("Detail rerenders!");
 
   return (
-    <div className='details'>
-      <img alt='horoscope name' src={sign.backgroundImg} />
+    <div className="details">
+      <img alt="horoscope name" src={sign.backgroundImg} />
       <h2>{sign.name}</h2>
       <h4>Element: {sign.element}</h4>
       <h4>Traits: {sign.traits}</h4>
